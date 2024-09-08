020687e495fd469d4d5bb56da2e41708dcf8f99d
Bekir Akok <bekir.akok@hotmail.com>
2024-09-08 12:47:58 +0300

    feat: initiate feed service

---
+++ src/pages/api/feed.js
@@ -0,0 +1,33 @@
+import clientPromise from "@/lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("MemeMaster");
+
+  if (req.method === "POST") {
+    try {
+      const competitions = await db
+        .collection("competitions")
+        .find({ status: "complete", winner: { $exists: true, $ne: null } })
+        .toArray();
+
+      const data = await Promise.all(
+        await competitions.map(async (competition) => {
+          const applies = await db
+            .collection("applies")
+            .countDocuments({ competationsId: competition?._id });
+
+          return { ...competition, applies };
+        })
+      );
+
+      res.status(200).json({ message: "Success", data });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
